87a8939b1b682aa2937e3efb1dba3d6ac14d564c
celiane <celi_bonifacio@hotmail.com>
2021-09-24 19:32:27 -0300

    Assimilado conceito de Map

--- function.js
+++ function.js
@@ -71,3 +71,32 @@ return total
 //arrow function
 const boaNoite=()=> console.log('Boa noite e boa sorte!!')
 //acima temos uma função anonima no formato arrow
+
+
+//callback - função Map
+
+const nums = [1,2,3,4,5]
+const dobro = n => n*2
+console.log(nums.map(dobro))
+
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia','Rafa']
+const primeiraLetra = texto =>texto[0]
+const letras = nomes.map(primeiraLetra)
+console.log(nomes,letras)
+
+const carrinho = [
+    {nome:'Caneta', qtde: 10, preco:7.99},
+    {nome:'Impressora', qtde: 0, preco:649.50},
+    {nome:'Caderno', qtde: 4, preco:27.99},
+    {nome:'Lapis', qtde: 3, preco:5.82},
+    {nome:'Tesoura', qtde: 1, preco:19.20},
+]
+// criar 2 funções de mapeamento - microdesafio
+
+const getNome = item => item.nome
+console.log (carrino.map(getNome))
+
+const getTotalValorPorItem = item => item.qtde * item.preco
+const total = carrinho.map(getTotalValorPorItem)
+console.log(getTotalValorPorItem);
+
